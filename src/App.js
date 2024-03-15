81da253ffdde70ff7a455da0d44a0bb286334d0d
Ihor Troianovskyi <igtroya555@gmail.com>
2024-03-15 15:03:18 +0200

    hw 73 completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import './App.css';
-import HeaderNav from "./components/HeaderNav";
-import AsideNav from "./components/AsideNav";
-import Content from "./components/Content";
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
-      <HeaderNav/>
-        <Container>
-            <Row className='mt-5'>
-                <Col xs lg="2" className='nav-background'><AsideNav/></Col>
-                <Col xs lg="8"><Content/></Col>
-            </Row>
-
-        </Container>
+      <Card/>
     </div>
   );
 }
