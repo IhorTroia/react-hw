27256f7aab9994167723b3ce7431d54319311c9d
Ihor Troianovskyi <igtroya555@gmail.com>
2024-03-15 15:13:20 +0200

    hw 74 completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
+      <Card title="First Title" />
 
+      <Card text="Second Body" />
+
+      <Card title="Third Title" text="Third Body" />
     </div>
   );
 }
