a2478d805986312fe54d1baa4e78213db9ffd179
Ihor Troianovskyi <igtroya555@gmail.com>
2024-03-21 09:56:32 +0200

    hw 80 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import MyCarousel from "./components/MyCarousel";
+import Container from 'react-bootstrap/Container';
+import carouselImage from "./bellamy2004.jpg";
 
 function App() {
   return (
     <div className="App">
-
+        <Container>
+            <MyCarousel image={carouselImage}/>
+        </Container>
     </div>
   );
 }
