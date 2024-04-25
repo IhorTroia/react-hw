be02482cdc80a0e46594d7964a6c36d6880b1367
Ihor Troianovskyi <igtroya555@gmail.com>
2024-04-25 10:17:12 +0300

    hw 88 completed

--- src/components/PostCatalog.jsx
+++ src/components/PostCatalog.jsx
@@ -1,44 +1,31 @@
-import React from "react";
+import {useEffect, useState} from "react";
 
-class PostCatalog extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: []
-        };
+function PostCatalog ()  {
+    const [data, setData] = useState([]);
+
+    async function fetchData() {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await res.json();
+        setData(data);
     }
 
-    async componentDidMount() {
-        await this.fetchData();
-    }
+    useEffect(  () => {
+        fetchData().catch(console.error)
+    })
 
-    fetchData = async () => {
-        try {
-            const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-            const data = await res.json();
-            this.setState({ data });
-        } catch (e) {
-            console.log(e)
-        }
-    }
 
-    render() {
-        const { data } = this.state
-
-        return (
-            <div className="posts">
-                <ul className="posts__list">
-                    {data.map(post => {
-                        return <li key={post.id} className="posts_single-post" data-post-id={post.id}>
-                            <h3 className="posts__post-title">{post.title}</h3>
-                            <p className="posts__post-description">{post.body}</p>
-                        </li>
-                    })}
-                </ul>
-            </div>
-        )
-
-    }
+    return  (
+        <div className="posts">
+            <ul className="posts__list">
+                {data.map(post => {
+                    return <li key={post.id} className="posts_single-post" data-post-id={post.id}>
+                        <h3 className="posts__post-title">{post.title}</h3>
+                        <p className="posts__post-description">{post.body}</p>
+                    </li>
+                })}
+            </ul>
+        </div>
+    )
 }
 
 export default PostCatalog;
